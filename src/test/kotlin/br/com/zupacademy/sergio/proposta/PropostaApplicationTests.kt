a07a06201d55e60185facb5314dee224b1353965
sergio <sergio.amorim@zup.com.br>
2021-07-14 19:32:02 -0300

    setup project (initial commit)
    - add spring project structure with initial dependencies, gitignore...
    - update the readme to something more meaningful

---
+++ src/test/kotlin/br/com/zupacademy/sergio/proposta/PropostaApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.zupacademy.sergio.proposta
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PropostaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
